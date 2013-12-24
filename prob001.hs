6534d199f86d1e03c1ccda30df560600de22517f
Shane Emmons <shane@emmons.io>
2013-12-24 06:55:10 -0800

    make prob001 runnable

--- prob001.hs
+++ prob001.hs
@@ -1,5 +1,10 @@
+module Main
+  where
+
 multiple_of_3 x = mod x 3 == 0
 multiple_of_5 x = mod x 5 == 0
 multiple_of_3_or_5 x = multiple_of_3 x || multiple_of_5 x
 
 result = sum $ filter multiple_of_3_or_5 [1..999]
+
+main = putStrLn $ show result
